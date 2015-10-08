1fc25e318a41cd782a5f28ec94060446f39698c7
Anjel0552 <anjelvillafranco0552@gmail.com>
2015-10-08 10:37:31 -0400

    Slider, DrawSomething, and Rainbow

---
+++ DrawSomething/DrawSomething/DrawView.swift
@@ -0,0 +1,22 @@
+//
+//  DrawView.swift
+//  DrawSomething
+//
+//  Created by Anjel Villafranco on 9/30/15.
+//  Copyright © 2015 Anjel Villafranco. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Rainbow/Rainbow/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Rainbow
+//
+//  Created by Anjel Villafranco on 10/1/15.
+//  Copyright © 2015 Anjel Villafranco. All rights reserved.
+//
+
+import Foundation
+
+@IBDesignable
+
+class rainbow: UIView {
+    
+}
